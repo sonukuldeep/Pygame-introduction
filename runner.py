2fea0404786987338b2256cd5e248f39d8666834
sonukuldeep <sonukuldeep@outlook.com>
2023-07-31 17:59:10 +0530

    rectangle added

---
+++ runner.py
@@ -0,0 +1,39 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
+# test_surface = pygame.Surface((100, 200))
+# test_surface.fill('Red')
+sky_surface = pygame.image.load("assets/graphics/Sky.png").convert()
+ground_surface = pygame.image.load("assets/graphics/ground.png").convert()
+text_surface = test_font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("assets/graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+snail_speed = -4
+
+player_surface = pygame.image.load(
+    "assets/graphics/Player/player_walk_1.png"
+).convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80, 300))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+    pygame.display.update()
+    snail_rect.left += snail_speed
+
+    if snail_rect.left < -100:
+        snail_rect.left = 800
+    clock.tick(60)
