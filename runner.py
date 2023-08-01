f006e1ea416e2a93a41d338001b204c999c584e9
sonukuldeep <sonukuldeep@outlook.com>
2023-08-01 21:31:28 +0530

    colors

--- runner.py
+++ runner.py
@@ -10,8 +10,8 @@ test_font = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
 # test_surface.fill('Red')
 sky_surface = pygame.image.load("assets/graphics/Sky.png").convert()
 ground_surface = pygame.image.load("assets/graphics/ground.png").convert()
-text_surface = test_font.render("My game", False, "Black")
-
+score_surface = test_font.render("My game", False, (64, 64, 64))
+score_rect = score_surface.get_rect(center=(400, 50))
 snail_surface = pygame.image.load("assets/graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
 snail_speed = -4
@@ -30,7 +30,9 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+    screen.blit(score_surface, score_rect)
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
 
